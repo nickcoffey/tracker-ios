363b31427cffa6b1be67da725c02958199131327
Nick Coffey <ncoffey@coolfiresolutions.com>
2020-11-05 12:33:04 -0600

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Nicholas Coffey on 11/5/20.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
